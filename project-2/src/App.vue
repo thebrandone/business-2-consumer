165d153267382c72f4077180147e986bb664b175
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-05-21 10:50:52 -0400

    Bootstrap Integration

--- project-2/src/App.vue
+++ project-2/src/App.vue
@@ -1,20 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
+  <div>
+  <b-navbar id="mainNav" toggleable="lg" type="light" >
+    <b-navbar-brand href="#">
+      <img src="https://i.ibb.co/nLNL8R4/B2C.png"  height=100 width=100 class="d-inline-block align-top" alt="Kitten">
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Customer</b-nav-item> 
+        <b-nav-item href="#">|  Vendor</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template slot="button-content"><em>User</em></template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+#nav-collapse{
+  font-size: 60px;
 }
 #nav {
   padding: 30px;
@@ -26,4 +54,8 @@
     }
   }
 }
+#mainNav{
+  background-color:rgb(217,68,50);
+}
+
 </style>
--- project-2/src/main.js
+++ project-2/src/main.js
@@ -1,10 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+
+
