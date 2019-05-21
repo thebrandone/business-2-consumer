ab0bd19323c6172660cf137b86c789172ae8fe8c
thebrandone <brandon.emons@gmail.com>
2019-05-20 20:32:28 -0400

    did installs, added models and server.js, created a geolocation test

--- project-2/src/views/Home.vue
+++ project-2/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <test/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import test from '@/components/test.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    test
   }
 }
 </script>
