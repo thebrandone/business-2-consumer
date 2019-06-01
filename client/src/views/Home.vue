4de21f6974143715a495acc6ac31ed7cdc73cde7
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-06-01 10:37:11 -0400

    Merging new Style/Images

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
-
   <div class="home">
-   
+    <img src="../assets/homeStock.png" width="100%">
+
+    <br>
+    <br>
+    <h1>Bridging the gap between Business and Customer!</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <strong>Copyright B2C 2019</strong>
   </div>
-  
 </template>
 
 <script>
 // @ is an alias to /src
-import test from '@/components/test.vue'
+import test from "@/components/test.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     test
   }
-}
+};
 </script>
