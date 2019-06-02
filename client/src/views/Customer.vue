c95d1c457066a4819b781f8a1c279ddbf6bfb5f8
Robert Tibbs <tibbsr09@gmail.com>
2019-06-02 15:49:44 -0400

    Login Validation
    Customer login functional except issue with incorrect login creditial info.  Vendor login set up similar to Customer login and will get from database but will not validate nor push to vendor content page.

--- client/src/views/Customer.vue
+++ client/src/views/Customer.vue
@@ -1,70 +1,107 @@
 <template>
   <div class="customer">
-    
-    <img src="@/assets/customerTop.png" width=100%>
-    
-    <div class="login">
-    <h1>Please login to your Customer account</h1>
-    <p>
-      <label for="username-input">
-        <span>Username:</span>
-        <input
-          v-model="username"
-          :disabled="loading"
-          type="text"
-          id="username-input"
-          name="username-input"
-        >
-      </label>
-    </p>
-    <p>
-      <label for="password-input">
-        <span>Password:</span>
-        <input
-          v-model="username"
-          :disabled="loading"
-          type="text"
-          id="password-input"
-          name="password-input"
-        >
-      </label>
-    </p>
-    <button type="submit">Login</button>
-    <a href="/#/newcustomer">  New Customer</a>
+    <img src="@/assets/customerTop.png" width="100%">
+
+    <div class="customerLogin">
+      <form @submit.prevent="validateCustomers">
+        
+      <h1>Please login to your Customer account</h1>
+      <b-alert
+        v-model="showBadAlert"
+        variant="danger"
+        dismissible
+      >User information is incorrect</b-alert>
+      <p>
+        <label for="username-input">
+          <span>Username:</span>
+          <input
+            v-model="username"
+            :disabled="loading"
+            type="text"
+            id="username-input"
+            name="username-input"
+          >
+        </label>
+      </p>
+      <p>
+        <label for="password-input">
+          <span>Password:</span>
+          <input
+            v-model="password"
+            :disabled="loading"
+            type="text"
+            id="password-input"
+            name="password-input"
+          >
+        </label>
+      </p>
+      <button type="submit">Login</button>
+      <a href="/#/newcustomer">New Customer</a>
+      </form>
+      
     </div>
-    <img src="@/assets/customerBottom.png" width=100%>
+    <img src="@/assets/customerBottom.png" width="100%">
   </div>
 </template>
 
 <script>
-import customercontent from '@/views/CustomerContent.vue';
+import axios from "axios";
+import router from "../router";
+import customercontent from "@/views/CustomerContent.vue";
 
 export default {
-  name: "customer",
-  components: {
-    customercontent
+  name: "customerLogin",
+
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+     validateCustomers: function() {
+      axios.get("/api/customers/").then(
+        function(customers) {
+          this.showGoodAlert = false;
+          this.showBadAlert = false;
+          for (
+            var checkUsername = 0;
+            checkUsername < customers.data.length;
+            checkUsername++
+          ) {
+            if (this.username === customers.data[checkUsername].username && this.password === customers.data[checkUsername].password) {
+              this.showGoodAlert = true;
+            } else {
+              this.showBadAlert = true;
+            }
+          }
+          if (this.showGoodAlert) {
+            this.moveCustomerPage();
+          }
+        }.bind(this)
+      );
+    },
+    moveCustomerPage: function() {
+      router.push(('/CustomerContent'))
+    }
   }
 };
-
-
 </script>
 
 <style>
-.CustomerContent{
+.CustomerContent {
   width: 80%;
 }
 
-.login{
+.login {
   border-width: 5px;
   border-color: black;
   border-style: solid;
   padding: 5px;
-  
 }
-.login a:hover{
+.login a:hover {
   color: red;
 }
-
 </style>
 
 
